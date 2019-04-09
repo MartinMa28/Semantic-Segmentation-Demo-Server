ff9ca759ab353669f5a78630373e0930356932d9
Martin <1135485226@qq.com>
2019-04-09 12:06:41 +0800

    finished the migration for postgres

--- segmentation_demo/settings.py
+++ segmentation_demo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'segmentation_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polls_db',
+        'USER': 'polls_admin',
+        'PASSWORD': 'abc123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
