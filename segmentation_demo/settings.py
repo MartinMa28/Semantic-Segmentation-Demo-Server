0148822c2da04a7cc72abb00c1670f46a9062f97
Martin <1135485226@qq.com>
2019-04-13 21:17:55 +0800

    included Bootstrap

--- segmentation_demo/settings.py
+++ segmentation_demo/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'polls_db',
         'USER': 'polls_admin',
         'PASSWORD': 'abc123',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
