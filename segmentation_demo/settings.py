1a9b77dc23ba0c446251e8519ba926a38a650937
Martin <1135485226@qq.com>
2019-04-13 12:15:05 +0800

    started the gallery app

--- segmentation_demo/settings.py
+++ segmentation_demo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'segmentation.apps.SegmentationGalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
