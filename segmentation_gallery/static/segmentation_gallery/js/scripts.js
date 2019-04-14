bcdebf7e6ea565212bb6d7cd4680b6f85ce7033e
Martin <1135485226@qq.com>
2019-04-14 21:32:45 +0800

    thumbnail gallery finished

---
+++ segmentation_gallery/static/segmentation_gallery/js/scripts.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.thumbnail-image').forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        const imgURL = thumbnail.dataset.url;
+        removeSelected();
+        displayImage(imgURL);
+        thumbnail.classList.add('selected');
+    });
+})
+
+const displayImage = (imgURL) => {
+    let image = document.querySelector('#image-container img')
+    image.src = imgURL;
+};
+
+const removeSelected = () => {
+    document.querySelectorAll('.thumbnail-image').forEach(thumbnail => {
+        thumbnail.classList.remove('selected');
+    })
+};
