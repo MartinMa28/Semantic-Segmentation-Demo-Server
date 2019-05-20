04472b997e75553f2cbbc279176b0a3440d3512c
Martin <1135485226@qq.com>
2019-05-20 18:16:52 +0800

    finish multiple nii files uploading

--- segmentation_gallery/static/segmentation_gallery/js/scripts.js
+++ segmentation_gallery/static/segmentation_gallery/js/scripts.js
@@ -1,19 +1,19 @@
-document.querySelectorAll('.thumbnail-image').forEach(thumbnail => {
-    thumbnail.addEventListener('click', () => {
-        const imgURL = thumbnail.dataset.url;
-        removeSelected();
-        displayImage(imgURL);
-        thumbnail.classList.add('selected');
-    });
-})
+// document.querySelectorAll('.thumbnail-image').forEach(thumbnail => {
+//     thumbnail.addEventListener('click', () => {
+//         const imgURL = thumbnail.dataset.url;
+//         removeSelected();
+//         displayImage(imgURL);
+//         thumbnail.classList.add('selected');
+//     });
+// })
 
-const displayImage = (imgURL) => {
-    let image = document.querySelector('#image-container img')
-    image.src = imgURL;
-};
+// const displayImage = (imgURL) => {
+//     let image = document.querySelector('#image-container img')
+//     image.src = imgURL;
+// };
 
-const removeSelected = () => {
-    document.querySelectorAll('.thumbnail-image').forEach(thumbnail => {
-        thumbnail.classList.remove('selected');
-    })
-};
+// const removeSelected = () => {
+//     document.querySelectorAll('.thumbnail-image').forEach(thumbnail => {
+//         thumbnail.classList.remove('selected');
+//     })
+// };
--- segmentation_gallery/views.py
+++ segmentation_gallery/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request):
-    ctx = {'greeting': 'Hello Gallery!'}
+    ctx = {}
+    if request.method == 'POST':
+        uploaded_files = request.FILES.getlist('nii_files')
+        ctx['files'] = []
+        fs = FileSystemStorage()
+        for f in uploaded_files:
+            fs.save(f.name, f)
+            ctx['files'].append(f.name)
+    
     return render(request, 'segmentation_gallery/index.html', context=ctx)
